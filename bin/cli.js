12ec662a4f55978b45b7aa526b5c6ae263f0291f
Scott Nath <github@scottnath.com>
2023-05-02 19:23:44 -0400

    :+1: add storybook cli builder

--- bin/cli.js
+++ bin/cli.js
@@ -11,11 +11,16 @@ program.command('add-storybook')
   .description('Add Storybook boilerplate to a project')
   .argument('[string]', 'project directory')
   .option('-t, --type <string>', 'project type')
+  .option('-b, --builder <string>', 'project builder')
   .action((str, options) => {
     const optionsArr = new Set();
     if (options.type) {
       optionsArr.add('--type');
       optionsArr.add(options.type);
+    }
+    if (options.builder) {
+      optionsArr.add('--builder');
+      optionsArr.add(options.builder);
     }
     addStorybookBoilerplate(str, Array.from(optionsArr));
   });
